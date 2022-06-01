afd0d1be08d3e7ff1de9a6ad840526585e3831cf
100hpfvr <mbellomo.p@gmail.com>
2022-06-01 18:34:03 -0300

    eita

--- front-end/src/app/screens/home/home.component.ts
+++ front-end/src/app/screens/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { BsModalService } from 'ngx-bootstrap/modal';
 
@@ -8,11 +8,13 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 	styleUrls: [ './home.component.scss' ]
 } )
 export class HomeComponent implements OnInit {
+	constructor ( bsModalService: BsModalService ) {
 
-	constructor (bsModalService: BsModalService) { }
+	}
+
 
 	@ViewChild( GoogleMap, { static: false } ) map: GoogleMap | any
-	@ViewChild( MapInfoWindow, { static: false } ) info: MapInfoWindow | any
+	@ViewChild( MapInfoWindow ) infoWindow!: MapInfoWindow;
 	options: google.maps.MapOptions = {
 		mapTypeId: 'roadmap',
 		zoomControl: false,
@@ -23,10 +25,60 @@ export class HomeComponent implements OnInit {
 	}
 
 
-	markers: any[] = [];
+	markers: any[] = [
+		{
+			
+			position: {
+				lat: -30.026274150043346,
+				lng: -51.22979358301162,
+			},
+			info: "ksksks",
+			label: {
+				color: 'Black',
+				text: 'rs',
+			},
+			title: 'Cãozinho Perdido',
+			options: {
+				animation: google.maps.Animation.BOUNCE,
+				// icon: '../../../assets/images/coelho.jpg'
+			},
+		},
+		{
+			position: {
+				lat: -30.025823633313102,
+				lng: -51.227953587627404,
+			},
+			info: "doguito",
+			label: {
+				color: 'Black',
+				text: 'gatito',
+			},
+			title: 'Cãozinho Perdido',
+			options: {
+				animation: google.maps.Animation.BOUNCE,
+				icon: this.createIconWithPhoto('../../../assets/images/cachorro.jpg')
+			},
+		},
+		{
+			position: {
+				lat: -30.02603263617451,
+				lng: -51.22881189451217,
+			},
+			info: "dogers",
+			label: {
+				color: 'Black',
+				text: 'Doggo Perdidasso',
+			},
+			title: 'Cãozinho Perdido',
+			options: {
+				animation: google.maps.Animation.BOUNCE,
+				//icon: '../../../assets/images/dog.jpg'
+			},
+		}
+
+	];
 	infoContent = '';
 	zoom = 18
-
 	center: any;
 
 	ngOnInit (): void {
@@ -35,44 +87,88 @@ export class HomeComponent implements OnInit {
 				lat: -30.026736275902316, //position.coords.latitude,
 				lng: -51.22842297420501,  //position.coords.longitude,
 				zoom: this.zoom,
-				
+
 			}
 		} )
 
-		this.addMarker( -30.026736275902316, -51.22842297420501 )
+		// this.addMarker( -30.026736275902316, -51.22842297420501 )
 
 	}
+
+	createIconWithPhoto (picture: any) {
+
+		var canvas = document.createElement( "canvas" );
+		canvas.width = 40;
+		canvas.height = 50;
+		var context = canvas.getContext( "2d" );
+		context!.fillStyle = "rgb(193,101,214)";
+		context!.beginPath();
+
+		context!.moveTo( 0, 0 );
+		context!.lineTo( 40, 0 );
+		context!.lineTo( 40, 40 );
+		context!.lineTo( 25, 40 );
+		context!.lineTo( 20, 50 );
+		context!.lineTo( 15, 40 );
+		context!.lineTo( 0, 40 );
+		context!.fill();
+
+		var img = new Image();
+		img.src = picture;
+		context!.drawImage( img, 2, 2, 36, 36 );
+		return canvas.toDataURL();
+	}
+
+
+
+
 
 	pegaMarcadorDoMouseClick ( event: google.maps.MapMouseEvent ) {
-		
-		console.log( event.latLng?.lat() )
-		this.addMarker( event.latLng?.lat(), event.latLng?.lng() )
+
+		console.log( event.latLng?.lat(), event.latLng?.lng() )
+		// this.addMarker(  )
 
 	}
-	addMarker ( latitudeDoClick: any, LongitudeDoClique: any ) {
+
+	teste () {
+		console.log( this.markers )
+	}
+
+	addMarker () {
 		this.markers.push( {
 			position: {
-				lat: latitudeDoClick,
-				lng: LongitudeDoClique,
+				lat: -30.02603263617451,
+				lng: -51.22881189451217,
 			},
-			
+			info: "doguito",
 			label: {
 				color: 'Black',
 				text: 'Doggo Perdidasso',
 			},
 			title: 'Cãozinho Perdido' + ( this.markers.length + 1 ),
-			info: 'Marker info ' + ( this.markers.length + 1 ),
+
 			options: {
 				animation: google.maps.Animation.BOUNCE,
+
 			},
+
 		} )
 	}
 
-	openInfo ( marker: MapMarker | any, content: any ) {
+	openInfo ( marker: MapMarker | HTMLElement, content: any ) {
 		this.infoContent = content
-		this.info.open()
-		
+		this.infoWindow.open()
+		console.log( 'teste' )
+
 	}
+
+	openInfoWindow ( marker: MapMarker ) {
+		this.infoWindow.open( marker );
+	}
+
+
+
+
 }
 
 
