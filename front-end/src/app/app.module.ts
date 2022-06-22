b16783e258f3a1f3c5b6ccc7b2da7bb65307d0a4
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2022-06-22 20:13:52 -0300

    Ajuste app.module.ts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { FormsModule } from '@angular/forms';
     LoginComponent,
     HomeComponent,
     ProfileComponent,
-RegisterUserComponent
+    RegisterUserComponent,
     RegistrosCasosComponent,
     GeolocationCardComponent, SignupComponent, PerdaFormComponent, EncontraFormComponent,ListarCasosComponent
   ],
